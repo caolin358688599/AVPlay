3e98b7e884f87e922bdb28f7af758e022ca02a05
caolin <18808058027@163.com>
2018-04-09 10:25:19 +0800

    初始化项目

---
+++ AVplayer/AVplayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVplayer
+//
+//  Created by 曹林 on 2018/4/9.
+//  Copyright © 2018年 caolin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
